0d0f4333bc4c4ce58e52c2176226941902a2e3d9
Adithya Biju <adithyabiju011@gmail.com>
2025-12-14 14:14:00 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+include 'db.php';
+
+$id = $_GET['id'] ?? 0;
+
+if($id == 0){
+    die("Invalid ID");
+}
+
+// Delete student
+$conn->query("DELETE FROM students WHERE id=$id");
+
+header("Location: index.php?msg=deleted");
+exit;
+?>
+
+
+
+
